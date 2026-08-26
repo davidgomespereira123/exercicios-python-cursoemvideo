a69235b11a0548f3fd4e2d39a4522c66cffd70a6
David <davidgomespereira111@gmail.com>
2026-08-26 18:28:25 -0300

    Adiciona explicações ao exe018 e inicia o exe022 (Manipulação de Texto)

---
+++ mundo_1/exe018.py
@@ -0,0 +1,14 @@
+import math
+
+angulo = float(input('Digite o ângulo que você deseja: '))
+
+# Converte o ângulo de graus para radianos
+radiano = math.radians(angulo)
+
+seno = math.sin(radiano)
+cosseno = math.cos(radiano)
+tangente = math.tan(radiano)
+
+print(f'O ângulo de {angulo}° tem o SENO de {seno:.2f}')
+print(f'O ângulo de {angulo}° tem o COSSENO de {cosseno:.2f}')
+print(f'O ângulo de {angulo}° tem a TANGENTE de {tangente:.2f}')
---
+++ mundo_1/exe022.py
@@ -0,0 +1,4 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Seu nome em maiusculo é: {}'.format(nome.upper()))
+print('Seu nome em minusculo é: {}'.format(nome.lower()))
+print('Seu nome possui {} letras'.format(len(nome)))
